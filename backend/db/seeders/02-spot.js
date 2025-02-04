e25f1884e71037fe2145e8146bd9fea3b0c9c841
esmeurena <esme.urena27@gmail.com>
2025-02-03 23:06:12 -0800

    added Spots seeder file, minor edit to Users seeder file, and changed spots.js to spot.js in migrations folder

---
+++ backend/db/seeders/02-spot.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const { Spot } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await Spot.bulkCreate([
+      {
+        ownerId: '123',
+        address: '123 First St',
+        city: 'San Diego',
+        state: 'California',
+        country: 'USA',
+        lat: 12.34,
+        lng: -12.34,
+        name: 'Airbnb Name',
+        description: 'Airbnb Name Description',
+        price: 123.45,
+        previewImage: 'https://www.image.com/',
+      },
+      {
+        ownerId: '1234',
+        address: '1234 Second St',
+        city: 'Los Angeles',
+        state: 'California',
+        country: 'USA',
+        lat: 12.44,
+        lng: -12.44,
+        name: 'Airbnb Second Name',
+        description: 'Airbnb Second Name Description',
+        price: 123.55,
+        previewImage: 'https://www.image.com/',
+      },
+      {
+        ownerId: '12345',
+        address: '12345 Third St',
+        city: 'San Francisco',
+        state: 'California',
+        country: 'USA',
+        lat: 12.54,
+        lng: -12.54,
+        name: 'Airbnb Third Name',
+        description: 'Airbnb Third Name Description',
+        price: 123.65,
+        previewImage: 'https://www.image.com/',
+      },
+    ], { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Spots';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      name: { [Op.in]: ['Airbnb Name', 'Airbnb Second Name', 'Airbnb Third Name'] }
+    }, {});
+  }
+};

edc5364443eb03658ca845530a94fa21555a15a4
WhiteyOne <Cayden.Hanks.Pro@gmail.com>
2025-02-17 14:52:56 -0800

    attempting a deployment fix

--- backend/db/migrations/02-spot.js
+++ backend/db/migrations/02-spot.js
@@ -17,7 +17,7 @@ module.exports = {
       userId: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        references: { model: 'Users', key: 'id' }
+        references: { model: 'Users', key: 'id', as:"Owner" }
       },
       address: {
         type: Sequelize.STRING(100),

368fa3bd3c56f808ca72e190f2eff6b43d6363d1
esmeurena <esme.urena27@gmail.com>
2025-03-12 15:22:10 -0700

    minor changes

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -58,6 +58,8 @@ router.post('/', validateLogin, async (req, res, next) => {
 
         const safeUser = {
             id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
             email: user.email,
             username: user.username,
         };
@@ -85,6 +87,8 @@ router.get('/', (req, res) => {
     if (user) {
         const safeUser = {
             id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
             email: user.email,
             username: user.username,
         };

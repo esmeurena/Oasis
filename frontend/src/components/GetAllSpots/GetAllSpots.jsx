f562cc9c8696d65d04b7a231e92470aaf299646d
esmeurena <esme.urena27@gmail.com>
2025-03-15 16:27:53 -0700

    getAllSpots completed

---
+++ frontend/src/components/GetAllSpots/GetAllSpots.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAllSpots } from '../../store/spots';
+import './GetAllSpots.css';
+
+function GetAllSpots() {
+  const dispatch = useDispatch();
+  //console.log("inside GetAllSpots: ", useSelector((state)=>state.spots.spots));
+  const spots = useSelector((state)=>state.spots.Spots);
+
+  useEffect(() => {
+    dispatch(fetchAllSpots());
+  }, [dispatch]);
+
+  if (!spots ||spots.length === 0) {
+    return <div>No spots found</div>;
+  }
+
+return (
+  <div>
+    <h1>Get All Spots</h1>
+    <ul>
+      {spots.map((spot) => (
+        <li key={spot.id}>
+          {spot.previewImage && (
+            <img src={spot.previewImage} className="spot-image"/>
+          )}
+          <h2>{spot.city}, {spot.state}</h2>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+}
+
+export default GetAllSpots;

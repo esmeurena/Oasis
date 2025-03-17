5c0b340dcc65d53a328c8121a0ef11767e6c0c0e
esmeurena <esme.urena27@gmail.com>
2025-03-16 19:52:03 -0700

    spacing in GetAllSpots

--- frontend/src/components/GetAllSpots/GetAllSpots.jsx
+++ frontend/src/components/GetAllSpots/GetAllSpots.jsx
@@ -7,39 +7,39 @@ import './GetAllSpots.css';
 function GetAllSpots() {
   const dispatch = useDispatch();
   //console.log("inside GetAllSpots: ", useSelector((state)=>state.spots.spots));
-  const spots = useSelector((state) => state.spots.Spots);
+  const spots = useSelector((state)=>state.spots.Spots);
 
   useEffect(() => {
     dispatch(fetchAllSpots());
   }, [dispatch]);
 
-  if (!spots) {
+  if(!spots) {
     return <h2>No spots found!!</h2>;
   }
 
-  return (
-    <div>
-      <h1>Get All Spots</h1>
-      <ul>
-        {spots.map((spot) => (
-          <Link key={spot.id} to={`/spots/${spot.id}`}>
-            <li>
-              {spot.previewImage && (
-                <img src={spot.previewImage} className="spot-image" />
-              )}
-              <h2>{spot.city}, {spot.state}</h2>
-              <h3>{spot.price} / night </h3>
+return (
+  <div>
+    <h1>Get All Spots</h1>
+    <ul>
+      {spots.map((spot) => (
+        <Link key={spot.id} to={`/spots/${spot.id}`}>
+        <li>
+          {spot.previewImage && (
+            <img src={spot.previewImage} className="spot-image"/>
+          )}
+          <h2>{spot.city}, {spot.state}</h2>
+          <h3>{spot.price} / night </h3>
 
-              <div className="tooltip">
-                {spot.city}, {spot.state}
-              </div>
+          <div className="tooltip">
+            {spot.city}, {spot.state}
+          </div>
 
-            </li>
-          </Link>
-        ))}
-      </ul>
-    </div>
-  );
+        </li>
+        </Link>
+      ))}
+    </ul>
+  </div>
+);
 }
 
 export default GetAllSpots;
